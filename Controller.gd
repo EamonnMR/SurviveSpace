b3553ff1a8709b17d003025a722495ddcca2de56
EamonnMR <Eamonn.m.r@gmail.com>
2021-04-07 18:29:42 -0400

    Futzing with trying to get rotation working again

--- Controller.gd
+++ Controller.gd
@@ -1,24 +1,20 @@
 extends Node
 
-var direction_change: float
+var rotation_change: float
 var shooting: bool
 var thrusting: bool
 var braking: bool
 
 func _physics_process(_delta: float):
-	direction_change = _get_direction_change()
+	rotation_change = _get_rotation_change()
 	shooting = Input.is_action_pressed("shoot")
 	braking = Input.is_action_pressed("brakes")
 	thrusting = Input.is_action_pressed("thrust") and not braking
 
-func _get_direction_change():
+func _get_rotation_change():
 	var dc = 0
 	if Input.is_action_pressed("turn_left"):
 		dc -= 1
 	if Input.is_action_pressed("turn_right"):
 		dc += 1
 	return dc
-
-func get_direction_change():
-	return direction_change
-	direction_change = 0
--- Player.gd
+++ Player.gd
@@ -4,6 +4,7 @@ var max_speed = 100
 var accel = 1
 var turn = 1
 const PLAY_AREA_RADIUS = 40000
+
 func get_limited_velocity_with_thrust():
 	var vel = get_linear_velocity()
 	if $Controller.thrusting:
@@ -20,8 +21,8 @@ func wrap_position_with_transform(state):
 		state.set_transform(transform)
 		
 func _integrate_forces(state):
-	rotate($Controller.get_direction_change() * turn)
-	set_applied_torque(0)  # Non-physics rotation
+	set_applied_torque($Controller.rotation_change)  # Non-physics rotation
+	print($Controller.rotation_change)
 	wrap_position_with_transform(state)
 	set_linear_velocity(get_limited_velocity_with_thrust())
 
