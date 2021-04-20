4ba999954c8dd9cf87534df79d934f79f2872182
EamonnMR <Eamonn.m.r@gmail.com>
2021-04-20 09:05:45 -0400

    Set up crafting UI frames

---
+++ ui/Crafting.gd
@@ -0,0 +1,3 @@
+extends Control
+onready var recipes_list = get_node("NinePatchPanel/MarginContainer/TabContainer/Craft/HBoxContainer/ScrollContainer/Recipes")
+onready var recipe_detail = get_node("NinePatchPanel/MarginContainer/TabContainer/Craft/HBoxContainer/Details")
