7ab66992b335880c7617d49bc143308665580f32
EamonnMR <Eamonn.m.r@gmail.com>
2021-07-14 20:49:30 -0400

    Engines have lights now

--- components/EngineEffects.gd
+++ components/EngineEffects.gd
@@ -3,7 +3,9 @@ extends Node2D
 func on():
 	for particle_emitter in get_children():
 		particle_emitter.emitting = true
+		particle_emitter.get_node("Light2D").show()
 
 func off():
 	for particle_emitter in get_children():
 		particle_emitter.emitting = false
+		particle_emitter.get_node("Light2D").hide()
